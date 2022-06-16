ece4061f90e0ba968d055ea4812ab47940447629
Andrzej Połetek <andrzej@poletek.net>
2022-06-16 11:26:08 +0200

    add a graphql library

--- car_auctions_backend/auctions/models.py
+++ car_auctions_backend/auctions/models.py
@@ -1,20 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Auction(models.Model):
-    ENGINE_POWER_UNIT = [
-        ('KM', 'Konie Mechaniczne'),
-        ('KW', 'Kilowaty'),
-    ]
     BODY_TYPES = [
         ('SEDAN', 'SEDAN'),
         ('HATCHBACK', 'HATCHBACK'),
         ('KOMBI', 'KOMBI')
-    ]
-    MILEAGE_TYPES = [
-        ('km', 'KILOMETRY'),
-        ('mile', 'MILE')
     ]
     PETROL_TYPES = [
         ('ELECTRIC', 'ELEKTRTYCZY'),
@@ -25,16 +18,22 @@ class Auction(models.Model):
         ('CNG', 'CNG'),
     ]
 
-    name = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)
     description = models.TextField(max_length=10000)
-    color = models.CharField(max_length=50)
     engine_power = models.IntegerField()
-    engine_power_unit = models.CharField(choices=ENGINE_POWER_UNIT, max_length=150)
     engine_capacity = models.IntegerField()
     type_body= models.CharField(choices=BODY_TYPES, max_length=150)
     production_year = models.IntegerField()
     mileage = models.IntegerField()
-    mileage_type = models.CharField(choices=MILEAGE_TYPES, max_length=150)
     petrol_type = models.CharField(choices=PETROL_TYPES, max_length=150)
+    start_price = models.DecimalField(decimal_places=2, max_digits=99)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    active = models.BooleanField(default=True)
 
 
+class PriceProposal(models.Model):
+    price = models.DecimalField(decimal_places=2, max_digits=99)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    auction = models.ForeignKey(Auction, on_delete=models.PROTECT)
+    datetime_propose = models.DateTimeField(auto_now_add=True)
